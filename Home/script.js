d024ef0eb98643fd752216e85f7c555c1c131bb6
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-04-01 17:24:43 +0530

    Add files via upload

---
+++ Home/script.js
@@ -0,0 +1,16 @@
+let userLogado = JSON.parse(localStorage.getItem("userLogado"))
+let logado = document.querySelector("#logado")
+
+logado.innerHTML = `Hi ${userLogado.nome},  Welcome to LogIn Page`
+
+if (localStorage.getItem("token") == null) {
+    alert("You must be logged in to be on this page")
+    window.location.href = "../index.html"
+
+}
+
+function sair() {
+    localStorage.removeItem('token')
+    localStorage.removeItem('userLogado')
+    window.location.href = "../index.html"
+}
